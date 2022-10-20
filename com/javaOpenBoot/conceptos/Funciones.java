da949ddcd89f0f8759ac6d9de07374303589555b
jcduque <pipem14@gmail.com>
2022-10-20 14:10:46 -0500

    clase funciones y parametros

--- com/javaOpenBoot/conceptos/Funciones.java
+++ com/javaOpenBoot/conceptos/Funciones.java
@@ -2,7 +2,31 @@ package com.javaOpenBoot.conceptos;
 
 public class Funciones {
     public static void main(String[] args) {
-        System.out.println("Esto es una prueba");
-        System.out.println("Esto es una prueba");
+//        System.out.println("Esto es una prueba");
+//        System.out.println("Esto es una prueba");
+//        String saludar = pruebaHola("Maria");
+//        System.out.println(saludar);
+//        holaMundo();
+//        bienvenida("Luis");
+        System.out.println(saludoAdicional());
+    }
+
+    private static String saludoAdicional() {
+        String saludo = "Buenas tardes";
+        return saludo;
+    }
+
+    private static void bienvenida(String nombre) {
+        System.out.println("Bienvenid@ " + nombre);
+    }
+
+    private static void holaMundo() {
+        System.out.println("Hola mundo");
+        System.out.println("Hola mundo");
+    }
+
+    private static String pruebaHola(String name) {
+        String saludo = "hola ";
+        return saludo + name;
     }
 }
