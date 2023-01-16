ef09699ead4e242c9ba67d254dfbbbf19201a92d
jcduque <pipem14@gmail.com>
2023-01-15 21:54:20 -0500

    Palabras reservadas continue y break

--- com/javaOpenBoot/conceptos/WhileLoop.java
+++ com/javaOpenBoot/conceptos/WhileLoop.java
@@ -3,9 +3,18 @@ package com.javaOpenBoot.conceptos;
 public class WhileLoop {
     public static void main(String[] args) {
         int count =0;
+        int countB =0;
         while (count <= 10){
             System.out.println("Hola mundo " + count);
             count ++;
+        }
+        while (countB < 6){
+            countB ++;
+            if (countB == 3){
+                //continue;// salta la ejecucion
+                break;// termina la ejecucion
+            }
+            System.out.println("Hola numero " + countB);
         }
         System.out.println("fin");
     }
