43ddea711080617354cbdd441c6b92413cd4e3b0
jcduque <pipem14@gmail.com>
2023-01-15 21:39:02 -0500

    Probando los ciclos while

--- com/javaOpenBoot/conceptos/ForLoop.java
+++ com/javaOpenBoot/conceptos/ForLoop.java
@@ -1,6 +1,6 @@
 package com.javaOpenBoot.conceptos;
 
-public class FoorLoop {
+public class ForLoop {
     public static void main(String[] args) {
         for (int i =0; i<=10; i++){
             System.out.println(i + " prueba");
---
+++ com/javaOpenBoot/conceptos/WhileLoop.java
@@ -0,0 +1,12 @@
+package com.javaOpenBoot.conceptos;
+
+public class WhileLoop {
+    public static void main(String[] args) {
+        int count =0;
+        while (count <= 10){
+            System.out.println("Hola mundo " + count);
+            count ++;
+        }
+        System.out.println("fin");
+    }
+}
