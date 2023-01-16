f2b49eb2e5661bb9607dca52afb112b86c7a2fb7
jcduque <pipem14@gmail.com>
2023-01-15 21:28:56 -0500

    Probando los ciclos for

---
+++ com/javaOpenBoot/conceptos/FoorLoop.java
@@ -0,0 +1,9 @@
+package com.javaOpenBoot.conceptos;
+
+public class FoorLoop {
+    public static void main(String[] args) {
+        for (int i =0; i<=10; i++){
+            System.out.println(i + " prueba");
+        }
+    }
+}
